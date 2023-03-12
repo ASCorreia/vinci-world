6ab118666982b2678c2d317ce5df995ecbc39e1c
ASCorreia <36857753+ASCorreia@users.noreply.github.com>
2023-03-12 23:12:00 +0000

    Added custom error files

---
+++ programs/vinci_world/src/error.rs
@@ -0,0 +1,9 @@
+use anchor_lang::prelude::*;
+
+#[error_code]
+pub enum CustomError {
+    #[msg("Insufficient Balance - SPL")]
+    InsufficientBalanceSpl,
+    #[msg("Insufficient Balance - SOL")]
+    InsufficientBalanceSol,
+}
